f75e6e79c4095a65175499fcbd28cbbd0552f707
ljj33 <ljj3347@naver.com>
2024-05-31 17:37:12 +0900

    240531

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <div class="w-full max-w-screen-md mx-auto p-4 md:py-8 ">
         <div class="sm:flex sm:items-center sm:justify-between">
           <a
-            href="https://flowbite.com/"
+            href="https://www.slayer99.com/"
             class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <img
